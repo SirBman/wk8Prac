26d158bd6f3a33cd18fe314f32c69314f7e7d6b1
Ben White <benjamin.white1@my.jcu.edu.au>
2016-09-12 10:06:29 +1000

    intermediate excercise with main file

---
+++ languages.py
@@ -0,0 +1,14 @@
+from Prac07.programmingLanguage import ProgrammingLanguage
+
+
+def main():
+
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(ruby)
+    print(python)
+    print(vb)
+
+main()
